288d88c96a4ed69a3ac47ffb635f651deb0ff866
btech240 <brenttlong@gmail.com>
2024-01-07 20:29:41 -0500

    Added functions to move and clear screen

--- main.py
+++ main.py
@@ -8,8 +8,31 @@ def move_foward():
     tim.forward(10)
 
 
+def move_backward():
+    tim.backward(10)
+
+
+def move_clockwise():
+    tim.right(10)
+
+
+def move_counterclockwise():
+    tim.left(10)
+
+
+def clear_screen():
+    tim.penup()
+    tim.clear()
+    tim.home()
+    tim.home()
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_foward)
+screen.onkey(key="w", fun=move_foward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="a", fun=move_counterclockwise)
+screen.onkey(key="c", fun=clear_screen)
 
 
 # KEEP AT BOTTOM OF CODE - exits the app on click
